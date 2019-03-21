1151ff356505fc79d97c8dfbca1d6c6fecd28ef0
william.lee <lyt0915@naver.com>
2019-03-21 18:26:07 +0900

    1. simply brute-forcely calculate. 2. (to be) back-tracking algorithm added.

--- app/src/main/java/com/example/tsudokucalculator/InputSudokuActivity.java
+++ app/src/main/java/com/example/tsudokucalculator/InputSudokuActivity.java
@@ -2,12 +2,142 @@ package com.example.tsudokucalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
 
-public class InputSudokuActivity extends Activity {
+public class InputSudokuActivity extends Activity implements View.OnClickListener {
+    private int row = 9;
+    private int column = 9;
+    TextView text[][];
+    int sudoku[][] = new int[9][9];
+    public int chooseX = 0;
+    public int chooseY = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_sudoku);
+
+        TableLayout table = findViewById(R.id.sudoku_content);
+        table.setShrinkAllColumns(true);
+        table.setStretchAllColumns(true);
+        TableRow.LayoutParams rowLayout = new TableRow.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        TableRow row[] = new TableRow[9];
+        this.text = new TextView[this.row][this.column]; // 데이터
+
+        for (int y = 0; y < this.row; y++) {
+            row[y] = new TableRow(this);
+
+            for (int x = 0; x < this.column; x++) {
+                this.text[y][x] = new Button(this);
+
+                this.text[y][x].setTextSize(15);
+                this.text[y][x].setId(y * 9 + x);
+                this.text[y][x].setOnClickListener(this);
+
+                this.sudoku[y][x] = 0;
+                row[y].addView(this.text[y][x]);
+            }
+            table.addView(row[y], rowLayout);
+        }
+
+        findViewById(R.id.input1).setOnClickListener(this);
+        findViewById(R.id.input2).setOnClickListener(this);
+        findViewById(R.id.input3).setOnClickListener(this);
+        findViewById(R.id.input4).setOnClickListener(this);
+        findViewById(R.id.input5).setOnClickListener(this);
+        findViewById(R.id.input6).setOnClickListener(this);
+        findViewById(R.id.input7).setOnClickListener(this);
+        findViewById(R.id.input8).setOnClickListener(this);
+        findViewById(R.id.input9).setOnClickListener(this);
+        findViewById(R.id.input_complete).setOnClickListener(this);
+        findViewById(R.id.input_clear).setOnClickListener(this);
+        findViewById(R.id.input_reset).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch ( id ){
+            case R.id.input1:
+                this.text[this.chooseY][this.chooseX].setText("1");
+                this.sudoku[this.chooseY][this.chooseX] = 1;
+                break;
+            case R.id.input2:
+                this.text[this.chooseY][this.chooseX].setText("2");
+                this.sudoku[this.chooseY][this.chooseX] = 2;
+                break;
+            case R.id.input3:
+                this.text[this.chooseY][this.chooseX].setText("3");
+                this.sudoku[this.chooseY][this.chooseX] = 3;
+                break;
+            case R.id.input4:
+                this.text[this.chooseY][this.chooseX].setText("4");
+                this.sudoku[this.chooseY][this.chooseX] = 4;
+                break;
+            case R.id.input5:
+                this.text[this.chooseY][this.chooseX].setText("5");
+                this.sudoku[this.chooseY][this.chooseX] = 5;
+                break;
+            case R.id.input6:
+                this.text[this.chooseY][this.chooseX].setText("6");
+                this.sudoku[this.chooseY][this.chooseX] = 6;
+                break;
+            case R.id.input7:
+                this.text[this.chooseY][this.chooseX].setText("7");
+                this.sudoku[this.chooseY][this.chooseX] = 7;
+                break;
+            case R.id.input8:
+                this.text[this.chooseY][this.chooseX].setText("8");
+                this.sudoku[this.chooseY][this.chooseX] = 8;
+                break;
+            case R.id.input9:
+                this.text[this.chooseY][this.chooseX].setText("9");
+                this.sudoku[this.chooseY][this.chooseX] = 9;
+                break;
+            case R.id.input_complete:
+                Intent intent = new Intent(InputSudokuActivity.this, OutputSudokuActivity.class);
+                int[] aa = new int[81];
+
+                for ( int y = 0; y < 9; y++ ){
+                    for ( int x = 0; x < 9; x++ ){
+                        aa[y*9 + x] = sudoku[y][x];
+                    }
+                }
+                intent.putExtra("text_parameter", aa);
+                startActivityForResult(intent, 1);
+
+                break;
+            case R.id.input_clear:
+                this.text[this.chooseY][this.chooseX].setText("");
+                this.sudoku[this.chooseY][this.chooseX] = 0;
+                break;
+            case R.id.input_reset:
+                for ( int y = 0; y < this.row; y++ ){
+                    for ( int x = 0; x < this.column; x++ ){
+                        this.text[y][x].setText("");
+
+                        this.sudoku[y][x] = 0;
+                    }
+                }
+                break;
+
+            default:
+                int y = id / 9;
+                int x = id % 9;
+
+                this.chooseX = x;
+                this.chooseY = y;
+                break;
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/tsudokucalculator/OutputSudokuActivity.java
@@ -0,0 +1,154 @@
+package com.example.tsudokucalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class OutputSudokuActivity extends Activity {
+    private int row = 9;
+    private int column = 9;
+
+    TextView text[][] = new TextView[9][9];
+
+    public class sudoku_item{
+        public int number = 0;
+        public boolean[] able = {false, true, true, true, true, true, true, true, true, true};
+    }
+
+    public int[] dx = { 0, 0, -1, 1 };
+    public int[] dy = { -1, 1, 0, 0 };
+
+    public void remove_able(int nowX, int nowY, sudoku_item[][] items){
+        items[nowY][nowX].able[items[nowY][nowX].number] = false;
+
+        for ( int d = 0; d < 4; d++ ){
+            int targetX = nowX + dx[d];
+            int targetY = nowY + dy[d];
+            while ( targetX >= 0 && targetX < 9 && targetY >= 0 && targetY < 9 ){
+                items[targetY][targetX].able[items[nowY][nowX].number] = false;
+                targetX = targetX + dx[d];
+                targetY = targetY + dy[d];
+            }
+        }
+
+        int initX = (nowX / 3) * 3;
+        int initY = (nowY / 3) * 3;
+        for ( int y = 0; y < 3; y++ ){
+            for ( int x = 0; x < 3; x++ ){
+                int targetX = initX + x;
+                int targetY = initY + y;
+                items[targetY][targetX].able[items[nowY][nowX].number] = false;
+            }
+        }
+
+    }
+
+    public void calculate_sudoku(int[][] sudoku){
+        sudoku_item[][] items = new sudoku_item[9][9];
+        for ( int y = 0; y < 9; y++ ) {
+            for (int x = 0; x < 9; x++) {
+                items[y][x] = new sudoku_item();
+            }
+        }
+        System.out.println("initing..");
+
+        for (int y = 0; y < 9; y++) {
+            for (int x = 0; x < 9; x++) {
+                items[y][x].number = sudoku[y][x];
+                remove_able(x, y, items);
+            }
+        }
+        while ( true ) {
+            int pre_count = 0;
+            int next_count = 0;
+
+            for (int y = 0; y < 9; y++) {
+                for (int x = 0; x < 9; x++) {
+                    remove_able(x, y, items);
+                    if ( items[y][x].number > 0 ){
+                        pre_count++;
+                    }
+                }
+            }
+
+            for (int y = 0; y < 9; y++) {
+                for (int x = 0; x < 9; x++) {
+                    check_item(items[y][x]);
+
+                    if ( items[y][x].number > 0 ){
+                        next_count++;
+                    }
+                }
+            }
+            System.out.println("check items..");
+
+            if ( pre_count == next_count) break;
+        }
+
+        for ( int y = 0; y < 9; y++ ){
+            for ( int x = 0; x < 9; x++ ){
+                sudoku[y][x] = items[y][x].number;
+            }
+        }
+        System.out.println("submitting..");
+    }
+
+    public void check_item(sudoku_item i){
+        int count = 0;
+        int idx = 0;
+        for ( int n = 1; n <= 9; n++ ){
+            if ( i.able[n] ) {
+                count++;
+                idx = n;
+            }
+        }
+        if ( count == 1 ) {
+            if ( i.number == 0 ) {
+                i.number = idx;
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_output_sudoku);
+
+        Intent intent = getIntent();
+        int[] tmp_sudoku = intent.getExtras().getIntArray("text_parameter");
+        int[][] sudoku = new int[9][9];
+
+        for (int i = 0; i < tmp_sudoku.length; i++) {
+            sudoku[i/9][i%9] = tmp_sudoku[i];
+        }
+
+        this.calculate_sudoku(sudoku);
+
+        TableLayout table = findViewById(R.id.output_sudoku_content);
+        table.setShrinkAllColumns(true);
+        table.setStretchAllColumns(true);
+        TableRow row[] = new TableRow[9];
+
+        TableRow.LayoutParams rowLayout = new TableRow.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        for (int y = 0; y < this.row; y++) {
+            row[y] = new TableRow(this);
+
+            for (int x = 0; x < this.column; x++) {
+                this.text[y][x] = new Button(this);
+
+                this.text[y][x].setTextSize(15);
+                this.text[y][x].setText(String.valueOf(sudoku[y][x]));
+
+                row[y].addView(this.text[y][x]);
+            }
+            table.addView(row[y], rowLayout);
+        }
+    }
+}
+
