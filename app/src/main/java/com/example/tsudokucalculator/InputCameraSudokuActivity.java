19e0e3c64e6fb21a6c042af45deb0a5be9af7c5e
william.lee <lyt0915@naver.com>
2019-03-22 19:13:54 +0900

    1. add camera activity

---
+++ app/src/main/java/com/example/tsudokucalculator/InputCameraSudokuActivity.java
@@ -0,0 +1,13 @@
+package com.example.tsudokucalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InputCameraSudokuActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_camera_sudoku);
+
+    }
+}
